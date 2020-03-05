ae42a36bdd266cf6acb2c7d7a641c00625941550
trumangift <trumangift@126.com>
2020-03-05 10:48:44 +0800

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('./'));
-app.listen(3000, () => {
-    console.log('express server started on http://localhost:3000');
-});
